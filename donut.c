2ae90081a97fc60c7aaa66df1490d997ac839af0
Ray Myers <ray.myers@morningstar.com>
2020-07-10 17:09:44 -0500

    var declarations.

--- donut.c
+++ donut.c
@@ -1,7 +1,13 @@
-k;
-double sin(), cos();
-main() {
-  float A = 0, B = 0, i, j, z[1760];
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main() {
+  float A = 0;
+  float B = 0;
+  float i;
+  float j; 
+  float z[1760];
   char b[1760];
   printf("\x1b[2J");
   for (;;) {
@@ -9,21 +15,28 @@ main() {
     memset(z, 0, 7040);
     for (j = 0; 6.28 > j; j += 0.07)
       for (i = 0; 6.28 > i; i += 0.02) {
-        float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A),
-              h = d + 2, D = 1 / (c * h * e + f * g + 5), l = cos(i),
-              m = cos(B), n = s\
-in(B),
-              t = c * h * g - f * e;
-        int x = 40 + 30 * D * (l * h * m - t * n),
-            y = 12 + 15 * D * (l * h * n + t * m), o = x + 80 * y,
-            N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+        float c = sin(i);
+        float d = cos(j);
+        float e = sin(A);
+        float f = sin(j);
+        float g = cos(A);
+        float h = d + 2;
+        float D = 1 / (c * h * e + f * g + 5);
+        float l = cos(i);
+        float m = cos(B);
+        float n = sin(B);
+        float t = c * h * g - f * e;
+        int x = 40 + 30 * D * (l * h * m - t * n);
+        int y = 12 + 15 * D * (l * h * n + t * m); 
+        int o = x + 80 * y;
+        int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
         if (22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
           z[o] = D;
           b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
         }
       }
     printf("\x1b[H");
-    for (k = 0; 1761 > k; k++)
+    for (int k = 0; 1761 > k; k++)
       putchar(k % 80 ? b[k] : 10);
     A += 0.04;
     B += 0.02;
